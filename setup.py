06a9282d1336333044b3dd10ac96a0ec3fadff3d
Aritra Biswas <pandalearnstocode@gmail.com>
2021-02-16 14:16:23 +0530

    created basic package structure files and folders

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ptolemaios",
+    version="0.0.1",
+    description="My private package from private github repo",
+    url="git@github.com:pandalearnstocode/ptolemaios.git",
+    author="Aritra Biswast",
+    author_email="pandalearnstocode@gmail.com",
+    license="unlicense",
+    packages=["ptolemaios"],
+    zip_safe=False,
+)
