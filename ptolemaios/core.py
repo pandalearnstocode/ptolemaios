a4ce03e85f2af1e9dc3e1bdfcfbfef1f4ad22f0b
Aritra Biswas <pandalearnstocode@gmail.com>
2021-02-16 14:30:15 +0530

    adding some basic functionality.

---
+++ ptolemaios/core.py
@@ -0,0 +1,2 @@
+def my_cool_test_method1():
+    return "It works!"
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from ptolemaios.core import my_cool_test_method1
+
+
+def test_my_cool_test_method1():
+    assert my_cool_test_method1() == "It works!"
