4112273243036325432db325a406d927a99f2036
Aritra Biswas <pandalearnstocode@gmail.com>
2021-02-16 14:35:34 +0530

    adding new functionality.

--- ptolemaios/core.py
+++ ptolemaios/core.py
@@ -1,2 +1,6 @@
 def my_cool_test_method1():
     return "It works!"
+
+
+def my_cool_test_method2():
+    return "It works too!"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,10 @@
 from ptolemaios.core import my_cool_test_method1
+from ptolemaios.core import my_cool_test_method2
 
 
 def test_my_cool_test_method1():
     assert my_cool_test_method1() == "It works!"
+
+
+def test_my_cool_test_method2():
+    assert my_cool_test_method2() == "It works too!"
